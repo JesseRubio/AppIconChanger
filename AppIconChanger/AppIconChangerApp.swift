6fecdaadc0ec4e026f5c7d904cef66ac710ea7d7
Jesse Rubio <jrubio87@hotmail.com>
2023-03-27 13:40:05 -0500

    Initial Commit

---
+++ AppIconChanger/AppIconChangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppIconChangerApp.swift
+//  AppIconChanger
+//
+//  Created by Jesse Rubio on 3/27/23.
+//
+
+import SwiftUI
+
+@main
+struct AppIconChangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
