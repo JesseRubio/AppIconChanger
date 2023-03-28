6c9f3821427ea9f27503709a1f969a505ed573d0
Jesse Rubio <jrubio87@hotmail.com>
2023-03-27 21:13:20 -0500

    Bugs fixed

--- AppIconChanger/ContentView.swift
+++ AppIconChanger/ContentView.swift
@@ -13,13 +13,15 @@ struct ContentView: View {
     
     var body: some View {
         VStack(alignment: .leading, spacing: 50) {
-            ForEach(AppIcon.allCases, id: \.self) {
-                appIcon in
+            ForEach(AppIcon.allCases, id: \.self) { appIcon in
                 Button(action: {
                     themeController.selectAppIcon(appIcon)
                 }) {
                     VStack {
-                        Image(appIcon.rawValue).resizable().frame(width: 50, height: 50, alignment: .center).cornerRadius(10)
+                        Image(appIcon.rawValue)
+                            .resizable()
+                            .frame(width: 50, height: 50, alignment: .center)
+                            .cornerRadius(10)
                         Text(appIcon.rawValue)
                     }
                 }
